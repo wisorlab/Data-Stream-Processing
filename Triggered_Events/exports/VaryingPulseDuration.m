cc1e4b697baac93a7f2448d67a8a80712face670
Jon Brennecke <brennecke.jonathan@wsu.edu>
2014-02-04 14:30:07 -0800

    working on VaryingPulseDuration.m

--- Triggered_Events/exports/VaryingPulseDuration.m
+++ Triggered_Events/exports/VaryingPulseDuration.m
@@ -1,28 +1,45 @@
 %=========================================================================
 % Main analysis script for the 'US Varying Pulse Duration' experiment
 %
-% created by @jonbrennecke
+% created by @jonbrennecke / http://github.com/jonbrennecke
 %=========================================================================
 
-offsets = {0.01;0.012;0.014;0.016;0.018;0.020;0.022;0.024};
+% predeclare the array of offsets
+% NOTE: these values were calculated by Michele Moore and Dr. Wisor as the 
+% best representation of the effect that we're looking at.
+offsets = [0.01 0.012 0.014 0.016 0.018 0.020 0.022 0.024];
 
-addpath ./etc/matlab-utils;
-utils = getUtils;
-[Excel, Workbooks, Sheets] = utils.xl.new();
-sheets = utils.xl.addSheets(Excel,{ 'Test' });
-utils.xl.set(sheets{1}, [1,1], { 'Offset Hz', 'Animal 1', 'Animal 2' });
-utils.xl.set(sheets{1}, [1,2], offsets);
+% add path to mcBinRead.m
+addpath ../../../Matlab/etc/;
 
+% add path to the XL class 
+addpath ../../../Matlab/etc/matlab-utils/;
+
+% instantiate an XL ActiveX object
+xl = XL;
+sheets = xl.addSheets({ 'Test' });
+xl.setCells(sheets{1}, [1,1], { 'Offset Hz', 'Animal 1', 'Animal 2' });
+xl.setCells(sheets{1}, [1,2], offsets');
 
 % open the file modal and get some '.raw' files
 [files,path] = uigetfile({'*raw','Binary MCS Files (*.raw)';'*.*','All Files'; },'Select Data File(s)','MultiSelect','On');
 if isstr(files), files = {files}; end
 
 for i=1:length(files)
-	[hdr,data] = mcBinRead([ path files{1} ]);
+
+	% import the data as 'matrix'
+	[hdr,data] = mcBinRead([ path files{i} ]);
 	t = 0:1/hdr.sampleRate:length(data)/hdr.sampleRate-1/hdr.sampleRate;
-    matrix = [t',data];
-    ttl = logical(matrix(:,2));
+    matrix = [ t', data ];
+
+    % the TTL track is the second column in 'matrix'
+    ttl = logical( matrix(:,2) );
+
+    % find TTL onsets
     onsets = find( diff(ttl) == 1 );
+	% onsets = matrix( diff(ttl) == 1, 1 );
+	
+    (ones(length(offsets),1) * onsets')' + (ones(length(onsets),1) * offsets )
+
 
 end
